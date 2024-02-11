376d29aa9d735f013ff91b418249792c6cd2edaf
motii <108280115+motii8128@users.noreply.github.com>
2024-02-11 11:08:11 +0900

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,103 @@
 extern crate hidapi;
-use hidapi::HidApi;
+use hidapi::{HidApi, HidDevice, HidError};
 
-pub struct DualShock4
+pub struct JoyStick
 {
-    api:HidApi,
     pub left_x:f32,
     pub left_y:f32,
     pub right_x:f32,
     pub right_y:f32,
+}
+
+pub struct Dpad
+{
     pub up_key:bool,
     pub down_key:bool,
     pub left_key:bool,
-    pub right_key:bool,
+    pub right_key:bool,   
+}
+
+pub struct Buttons
+{
+    pub circle:bool,
+    pub cross:bool,
+    pub triangle:bool,
+    pub cube:bool,
+    pub r1:bool,
+    pub r2:bool,
+    pub l1:bool,
+    pub l2:bool,
+}
+
+pub struct DualShock4
+{
+    device:HidDevice,
+    pub sticks:JoyStick,
+    pub dpad:Dpad,
+    pub btns:Buttons,
 }
 
 impl DualShock4 {
-    pub fn new()
+    pub fn new()->DualShock4
     {
         let api = HidApi::new().unwrap();
 
         let dev = api.open(1356, 2508).unwrap();
-    }
-    pub fn get_data(buf:&[u8])
-    {
-        let l_x = map(buf[1], 0.0, 255.0, -1.0, 1.0);
-        let l_y = map(buf[2], 0.0, 255.0, -1.0, 1.0);
-        let r_x = map(buf[3], 0.0, 255.0, -1.0, 1.0);
-        let r_y = map(buf[4], 0.0, 255.0, -1.0, 1.0);
 
-    }   
+        let joy = JoyStick{
+            left_x:0.0,
+            left_y:0.0,
+            right_x:0.0,
+            right_y:0.0
+        };
+
+        let pad = Dpad{
+            up_key:false,
+            down_key:false,
+            left_key:false,
+            right_key:false
+        };
+
+        let btn = Buttons{
+            circle:false,
+            cross:false,
+            triangle:false,
+            cube:false,
+            r1:false,
+            r2:false,
+            l1:false,
+            l2:false
+        };
+
+        DualShock4
+        {
+            device:dev,
+            sticks:joy,
+            dpad:pad,
+            btns:btn
+        }
+    }
+    pub fn read(&mut self)->Result<(), HidError>
+    {
+        let mut buf = [0_u8;256];
+        match self.device.read(&mut buf) {
+            Ok(size)=>{
+                let get_data = &buf[..size];
+
+                let (j, btn, d) = convert(get_data);
+
+                self.sticks = j;
+                self.btns = btn;
+                self.dpad = d;
+
+                Ok(())
+            }
+            Err(e)=>{
+                return Err(e)
+            }
+        }
+    }
+    
 }
 
 fn map(value:u8,in_min:f32, in_max:f32, out_min:f32, out_max:f32)->f32
@@ -42,3 +111,52 @@ fn map(value:u8,in_min:f32, in_max:f32, out_min:f32, out_max:f32)->f32
 
     result
 }
+
+fn convert(buf:&[u8])->(JoyStick, Buttons, Dpad)
+    {
+        let l_x = map(buf[1], 0.0, 255.0, -1.0, 1.0);
+        let l_y = map(buf[2], 0.0, 255.0, 1.0, -1.0);
+        let r_x = map(buf[3], 0.0, 255.0, -1.0, 1.0);
+        let r_y = map(buf[4], 0.0, 255.0, 1.0, -1.0);
+
+        let joy = JoyStick{left_x:l_x,left_y:l_y,right_x:r_x,right_y:r_y};
+        let mut btns = Buttons{
+            circle:false,
+            cross:false,
+            triangle:false,
+            cube:false,
+            r1:false,
+            r2:false,
+            l1:false,
+            l2:false,
+        };
+
+        let mut dpad = Dpad{
+            up_key:false,
+            down_key:false,
+            right_key:false,
+            left_key:false
+        };
+
+        match buf[5] {
+            0=>dpad.up_key = true,
+            2=>dpad.right_key = true,
+            4=>dpad.down_key = true,
+            6=>dpad.left_key = true,
+            24=>btns.cube = true,
+            40=>btns.cross = true,
+            72=>btns.circle = true,
+            136=>btns.triangle = true,
+            8=>(),
+            _=>()
+        }
+
+        match buf[6] {
+            1=>btns.l1 = true,
+            2=>btns.r1 = true,
+            4=>btns.l2 = true,
+            8=>btns.r2 = true,
+            _=>(),
+        }
+        (joy, btns, dpad)
+    }   
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use async_std;
-
+use dualshock_driver::DualShock4;
 
 
 #[async_std::main]
 async fn main()
 {
-    
+    let mut ds = DualShock4::new();
+
+    loop {
+        let _ = ds.read();
+        println!("Stick:{{left_x:{}, left_y:{}, right_x:{}, right_y:{}}}", ds.sticks.left_x, ds.sticks.left_y, ds.sticks.right_x, ds.sticks.right_y);
+    }
 }
